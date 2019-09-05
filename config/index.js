f612763b983a3800f23c777c97157a34661d7315
vatashadow <1553237892@qq.com>
2019-09-05 14:41:40 +0800

    fix config

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/usr/share/nginx/html',
     proxyTable: {},
 
     // Various Dev Server settings
