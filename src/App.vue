8853889e70fd42c4ad188ff93910632cc9164ab6
vatashadow <1553237892@qq.com>
2019-09-05 15:45:52 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default {
   beforeCreate () {
     if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
       // TODO 待域名确定修改
-      window.location.href="//baidu.com";
+      window.location.href="//sharpfactory.top";
     }
   }
 }
